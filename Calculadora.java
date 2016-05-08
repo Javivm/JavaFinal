974a095c6c2afd203ac013808a14149631abcb4e
Javier <javier.v.m.1994@gmail.com>
2016-05-08 01:24:06 -0500

    Ya calculadora completa con UI

--- Calculadora.java
+++ Calculadora.java
@@ -1,39 +1,163 @@
+/**
+ * 
+ * 
+ * @author Armando Azar
+ * @author Santiago Menendez
+ * @author Luis Padilla
+ * @author Javier Vega
+ */
 
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import java.lang.*;
+import java.awt.event.*;
+import java.awt.GridLayout;
+import javax.swing.border.LineBorder;
+import java.awt.Color;
+import java.awt.*;
 
-public class Calculadora{
-	/*
-	Obtener peso * .4536
-	Obtener Altura
-	Obtener Gender (Masc = 5 Fem = 1)
+public class Calculadora extends JFrame{
+    
+    private JTextField pesoTF = new JTextField(20);
+    private JTextField edadTF = new JTextField(20);
+    private ButtonGroup sexo = new ButtonGroup();
+    private JRadioButton mascRB = new JRadioButton("Masculino");
+    private JRadioButton femRB = new JRadioButton("Femenino");
+    private JButton calc = new JButton("Calcular");
+    private JButton calcPerfil = new JButton("Calcular con Mi Perfil");
+    private JButton regresar = new JButton("Regresar");
+    private JLabel calculadora =new JLabel("Calculadora");
+    private JLabel edad =new JLabel("Edad:");
+    private JLabel genero = new JLabel("Genero:");
+    private JLabel peso = new JLabel("Peso:");
+	
+    public JFrame frame = new JFrame();
+    LineBorder line=new LineBorder(Color.black,3,true);
+    
+    
+    
+    /**
+     * 
+     * 
+     * @author Armando Azar
+     * @author Santiago Menendez
+     * @author Luis Padilla
+     * @author Javier Vega
+     */
+    
+    public Calculadora(){
+		
+		frame.setVisible(true);
+		
+		sexo.add(mascRB);
+	    sexo.add(femRB);
+		
+	    JPanel content = new JPanel();
+	    frame.add(content);
+		content.setBorder(new EmptyBorder(20, 20, 20, 20));
+	 	content.setLayout(new GridLayout(11, 1, 0, 5));
+		content.add(calculadora);
+		content.add(peso);
+		content.add(pesoTF);
+		content.add(edad);
+		content.add(edadTF);
+		content.add(genero);
+		content.add(mascRB);
+		content.add(femRB);
+		content.add(calc);
+		content.add(calcPerfil);
+		content.add(regresar);
+		
+		//Propiedades
+		//Borde de los Botones
+		pesoTF.setBorder(line);
+		edadTF.setBorder(line);
+		calc.setBorder(line);
+		calcPerfil.setBorder(line);
+		regresar.setBorder(line);
+		
+	       	//Color de los Botones
+		calc.setBackground(new Color(255,119,10));
+		calcPerfil.setBackground(new Color(255,119,10));
+		regresar.setBackground(new Color(255,119,10));
+		
+		//Color de fondo
+		frame.setBackground(new Color(230,230,230));
 
-	MaxBPM = 210 - (Age / 2) - (0.5 * weight) + Gender - 1
+		calculadora.setFont(new Font("Serif",Font.PLAIN,50));
+		peso.setFont(new Font("Serif",Font.PLAIN,20));
+		edad.setFont(new Font("Serif",Font.PLAIN,20));
+		genero.setFont(new Font("Serif",Font.PLAIN,20));
+		pesoTF.setFont(new Font("Serif",Font.PLAIN,20));
+		edadTF.setFont(new Font("Serif",Font.PLAIN,20));
+		
+		//Propiedades de la Ventana
+		this.setSize(500,700);
+		this.setResizable(false);
+		this.setLocationRelativeTo(null);	
+		this.setContentPane(content);
+		this.pack();
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setTitle("Actividades");
+		this.setVisible(true);
 
-	Zone 1 = low value BPM * 0.5 high value BPM * 0.6 
-	Zone 2 = low value BPM * 0.6 high value BPM * 0.7
-	Zone 3 = low value BPM * 0.7 high value BPM * 0.8
-	Zone 4 = low value BPM * 0.8 high value BPM * 0.9
-	Zone 5 = low value BPM * 0.9 high value BPM * 0.95
-	*/
-	private static int edad, gender, peso;
-	private static double maxBPM;
-	private static String sexo;
+		
+		calc.addActionListener(new CalcListener());
+		calcPerfil.addActionListener(new CalcPerfilListener());
+		regresar.addActionListener(new RegresarListener());
+		
+		this.setContentPane(content);
+		this.pack();	    
+    }
 
+	    /**
+	     * 
+	     * 
+	     * @author Armando Azar
+	     * @author Santiago Menendez
+	     * @author Luis Padilla
+	     * @author Javier Vega
+	     */
 
-	public static void main(String[] args){
+	class CalcListener implements ActionListener{
+	    public void actionPerformed(ActionEvent e){
+	    	int edad, peso;
+	    	double maxBPM;
+	    	double[] zona1 = new double[2];
+	    	double[] zona2 = new double[2];
+	    	double[] zona3 = new double[2];
+	    	double[] zona4 = new double[2];
+	    	double[] zona5 = new double[2];
+	    	String sexo = "", sedad, speso;
+	    	sedad = edadTF.getText();
+	    	speso = pesoTF.getText();
+			edad = Integer.parseInt(sedad);
+			peso = Integer.parseInt(speso);
 
-		sexo = "Masculino";
-		edad = 15;
-		peso = 70;
-
-		if(sexo.equals("Masculino")){
-			gender = 4;
-		}else{
-			gender = 0;
-		}
-
-		maxBPM = (210 - ((double)edad / 2) - (0.05 * ((double)peso*.04536)) + gender) - 2;
-
-		System.out.println(maxBPM);
+			if(mascRB.isSelected() == true)
+				sexo = "Masculino";
+			if(femRB.isSelected() == true)
+				sexo = "Femenino";
+			maxBPM = CalculadoraModel.calculaBPM(edad, peso, sexo);
+			zona1 = CalculadoraModel.calculaZona1();
+			zona2 = CalculadoraModel.calculaZona2();
+			zona3 = CalculadoraModel.calculaZona3();
+			zona4 = CalculadoraModel.calculaZona4();
+			zona5 = CalculadoraModel.calculaZona5();
+			new MainResultados(maxBPM, zona1, zona2, zona3, zona4, zona5);
+	    }
 	}
 
+	class CalcPerfilListener implements ActionListener{
+	    public void actionPerformed(ActionEvent e){
+	    	
+	    }
+	}
+
+	class RegresarListener implements ActionListener{
+	    public void actionPerformed(ActionEvent e){
+		
+	    }
+	}
 }
+    
